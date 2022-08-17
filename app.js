6ebafe763f70e6859092590b9faf2703b99afcd2
Charlie Pariente <charlesmapariente@gmail.com>
2022-08-17 10:19:55 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+function removeTransition(e) {
+    if (e.propertyName != 'transform') return;
+    this.classList.remove('playing');
+};
+
+const pads = document.querySelectorAll('.pad');
+pads.forEach(pad => pad.addEventListener('transitionend', removeTransition));
+
+pads.forEach(pad => pad.addEventListener('click', (e) => {
+    const eCode = e.target.attributes[0].value
+    const audio = document.querySelector(`audio[data-key="${eCode}"]`);
+    audio.volume = 0.5;
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    pad.classList.add('playing');
+}));
+
+function keyPlay(e) {
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const pad = document.querySelector(`.pad[data-key="${e.code}"]`);
+    if (e.code === 'space') e.preventDefault();
+    audio.volume = 0.5;
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    pad.classList.add('playing');
+};
+
+document.addEventListener('keydown', keyPlay);
