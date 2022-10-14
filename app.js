dc7f5821201a694a7e181359fefb07856b14b7a4
Charlie Pariente <charlesmapariente@gmail.com>
2022-10-14 11:05:30 -0700

    Add CSS styles to drum machine. Add Header and Footer sections to HTML. Fix bugs in app.js to disable spacebar scroll.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ pads.forEach(pad => pad.addEventListener('transitionend', removeTransition));
 pads.forEach(pad => pad.addEventListener('click', (e) => {
     const eCode = e.target.attributes[0].value
     const audio = document.querySelector(`audio[data-key="${eCode}"]`);
-    audio.volume = 0.5;
+    audio.volume = 0.3;
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
@@ -19,8 +19,8 @@ pads.forEach(pad => pad.addEventListener('click', (e) => {
 function keyPlay(e) {
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
     const pad = document.querySelector(`.pad[data-key="${e.code}"]`);
-    if (e.code === 'space') e.preventDefault();
-    audio.volume = 0.5;
+    if (e.code === 'Space') e.preventDefault();
+    audio.volume = 0.3;
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
